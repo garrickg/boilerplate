1fc0bc0aa25ddf8f650f312ea600c6655c7510a5
Garrick <garrickgunn@gmail.com>
2017-06-12 20:40:37 -0700

    Delete base.js

--- src/data/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base'
-
-const base = Rebase.createClass({
-  apiKey: 'AIzaSyC_EJVqyhY_7xa6yGSlFnWlMTODdnWjVD0',
-  authDomain: 'boilerplate-5d240.firebaseapp.com',
-  databaseURL: 'https://boilerplate-5d240.firebaseio.com'
-})
-
-export default base
